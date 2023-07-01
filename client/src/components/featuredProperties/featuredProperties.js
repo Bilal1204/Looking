30512475f46f49f26816dfd6a8bf2b3454c9d81d
Bilal <bilalshaikh04@outlook.com>
2023-07-01 15:38:45 +0530

    new client build

--- client/src/components/featuredProperties/featuredProperties.js
+++ client/src/components/featuredProperties/featuredProperties.js
@@ -14,7 +14,7 @@ const FeaturedProperties = () => {
         {
           data?.map((item,i) =>
         <div className="fpItem" key={i}>
-        <img className='fpImg' src={item.photos[0]} alt="" />
+        <img className='fpImg' src={item.photos[0] || "https://cf.bstatic.com/xdata/images/hotel/square600/187853972.webp?k=dd753431cf4b638642614dc060512d832d24f3249eef30b9a5f18601d2ac5635&o="} alt="" />
         <span className="fpName">{item.name}</span>
         <span className="fpCity">{item.city}</span>
         <span className="fpPrice">Hotel starts from {item.cheapestPrice}</span>
